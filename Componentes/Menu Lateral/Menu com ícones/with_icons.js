d2724dc94ea554a660c3329ba5ff0cc27e3801f7
AMA | Equipa de Usabilidade e Planeamento de Sistemas <andre.zibaia@ama.pt>
2017-06-28 11:37:17 +0100

    changes in framework

--- Componentes/Menu Lateral/Menu com ícones/with_icons.js
+++
@@ -1,9 +0,0 @@
-$(document).on("click", ".side-menu > ul > li", function(e){
-    if($(this).find('.sub-menu').length>0){
-        e.preventDefault();
-        e.stopPropagation();
-        $(this).toggleClass('active');
-        $(this).find('.sub-menu').stop().slideToggle();
-    }
-    
-});
